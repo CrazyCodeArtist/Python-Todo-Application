b3db2078e0b8255d80fdc757df15e5ea6cf02cab
Muhammad Ahmed <clanofdevelopers@gmail.com>
2024-12-22 11:57:36 -0800

    Version One

--- todo.py
+++ todo.py
@@ -3,21 +3,21 @@ from tkinter.font import Font
 
 root = Tk()
 root.title("TO DO List")
-root.geometry("500x500")
+root.geometry("500x600")
 
 
-my_font = Font(
+customFont = Font(
     family ="Bradley Hand ITC",
-    size = 30,
+    size = 20,
     weight = 'bold'
 )
 
-my_frame = Frame(root)
-my_frame.pack(pady = 10)
+taskFrame = Frame(root)
+taskFrame.pack(pady = 10,padx=10 )
 
 
-my_list = Listbox(my_frame,
-font=my_font,
+todoListbox = Listbox(taskFrame,
+font=customFont,
 width = 25,
 height = 5,
 bg = "SystemButtonFace",
@@ -29,10 +29,83 @@ activestyle = "none"
 
 
 )
-my_list.pack()
-stuff = ["Ttask one ","Eat In ","Hello","task Last"]
-for item in stuff:
-    my_list.insert(END,item)
+
+
+ComplettedTodoListbox = Listbox(root,
+font=customFont,
+width = 25,
+height = 5,
+bg = "SystemButtonFace",
+bd = 0,#border = 0    
+fg="#464646",
+highlightthickness =0,
+selectbackground = "#a6a6a6",
+activestyle = "none"
+
+
+)
+
+
+todoListbox.pack()
+ComplettedTodoListbox.pack()
+
+ComplettedTodoArray = ["one" , "two"]
+
+todoTaskArray = ["Ttask one ","Eat In ","Hello","task Last"]
+
+for item in todoTaskArray:
+    todoListbox.insert(END,item)
+
+for item in ComplettedTodoArray:
+    ComplettedTodoListbox.insert(END,item)
+
+my_entry = Entry(root, font=("Bradley Hand ITC",20))
+my_entry.pack(pady=20)
+
+button_frame = Frame(root)
+button_frame.pack(pady=20)
+
+def delete_item():
+    todoListbox.delete(ANCHOR)
+    
+
+def add_item():
+    todoListbox.insert(END,my_entry.get())
+
+def addCompletedTaskToList():
+    selected_task = todoListbox.get(ANCHOR)
+    if selected_task:
+        ComplettedTodoListbox.insert(0, selected_task)
+        todoListbox.delete(ANCHOR)
+
+
+def delComplettedTask():
+    ComplettedTodoListbox.delete(0,END)
+    
+
+
+deleteBtn = Button(button_frame, text ="Delete " ,command=delete_item)
+
+addBtn = Button(button_frame, text ="Add Item ",command= add_item)
+
+completedTaskBtn = Button(button_frame, text ="Mark As Completed",command= addCompletedTaskToList)
+
+
+delComplettedTaskBtn = Button(button_frame, text ="Delete All Completed Task",command= delComplettedTask)
+
+
+
+
+
+
+
+
+deleteBtn.grid(row=0,column=0)
+addBtn.grid(row=0,column=1,padx=20)
+completedTaskBtn.grid(row=0,column=2)
+delComplettedTaskBtn.grid(row=0,column=3,padx=20)
+
+
 
 
 root.mainloop()
